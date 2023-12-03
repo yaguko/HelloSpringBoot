9a404b9bf19806c6adb67b41ec13a64194be9a15
yaguko <yaguko_7@yahoo.co.jp>
2023-12-03 17:22:39 +0900

    14章

--- src/main/java/com/techacademy/HelloSpringBootApplication.java
+++ src/main/java/com/techacademy/HelloSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelloSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpringBootApplication.class, args);
+    }
 
 }
--- src/main/java/com/techacademy/HelloSpringBootController.java
+++ src/main/java/com/techacademy/HelloSpringBootController.java
@@ -1,10 +1,9 @@
 package com.techacademy;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime; // 追加
+import java.time.format.DateTimeFormatter; // 追加
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable; // 追加
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,19 +14,12 @@ public class HelloSpringBootController {
         return "Hello SpringBoot!";
     }
 
+    // ----- 追加:ここから -----
     @GetMapping("now")
     public String dispTime() {
         String now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
 
         return "現在時刻：" + now;
-    }
-
-    // ----- 追加:ここから -----
-    @GetMapping("/plus/{val1}/{val2}")
-    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
-        int res = 0;
-        res = val1 + val2;
-        return "計算結果：" + res;
     }
     // ----- 追加:ここまで -----
 }
